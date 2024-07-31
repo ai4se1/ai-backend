ae4f471fd4f71a320d677bf19e4c179782f034dd
Alexander Sohn <39831045+Sohn123@users.noreply.github.com>
2024-07-31 12:29:09 +0200

    Update run.sh to use prebuilt docker container

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-#!/env/bin/bash
-docker run -d --rm --name ai4se-api-test-1 --gpus "device=1" -v /var/opt/huggingface:/root/.cache/huggingface -p0.0.0.0:8010:8000 ai4se-1-api
+#!/bin/env bash
+docker run -d --rm --name ai4se-1-api --gpus "device=1" -v /var/opt/huggingface:/root/.cache/huggingface -p0.0.0.0:8010:8000 ghcr.io/ai4se1/ai-backend:main
